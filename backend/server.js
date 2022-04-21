dfe9dd6216dda713cda4efcd8c08e47e980f419b
wildanvin <danvin.jim@gmail.com>
2022-04-21 00:36:01 -0500

    Routes and Controller is Express

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-const express = require("express")
-const dotenv = require("dotenv").config()
+const express = require('express')
+const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome my fren" })
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome my fren' })
 })
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
