a985c93b22169304cd521aeb4da3b4bcdf643979
wildanvin <danvin.jim@gmail.com>
2022-04-22 23:53:21 -0500

    JWT Basi Implementation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
 //@desc Register a new user
@@ -37,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -48,8 +50,29 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route /api/users/login
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login Route')
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+
+  //Check user and password is matched
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid credentials')
+  }
 })
+
+//Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d', // expires in 30 days
+  })
+}
 
 module.exports = {
   registerUser,
