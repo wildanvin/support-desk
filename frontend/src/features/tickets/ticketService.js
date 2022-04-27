284655c50495858148ad908bc8a65f5c42e455de
wildanvin <danvin.jim@gmail.com>
2022-04-26 23:48:38 -0500

    Listing Tickets in UI

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -11,13 +11,27 @@ const createTicket = async (ticketData, token) => {
     },
   }
 
-  const response = axios.post(API_URL, ticketData, config)
+  const response = await axios.post(API_URL, ticketData, config)
 
   return response.data
 }
 
+//Get user Tickets
+
+const getTickets = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+
+  const response = await axios.get(API_URL, config)
+
+  return response.data
+}
 const ticketService = {
   createTicket,
+  getTickets,
 }
 
 export default ticketService
