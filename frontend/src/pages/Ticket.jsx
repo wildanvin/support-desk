1e78a59a9bf5c1baf5757c5fc12dba8488073924
wildanvin <danvin.jim@gmail.com>
2022-04-27 00:40:51 -0500

    Single Ticket View

---
+++ frontend/src/pages/Ticket.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import Modal from 'react-modal'
+import { FaPlus } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { getTicket, reset } from '../features/tickets/ticketSlice'
+// import {
+//   getNotes,
+//   createNote,
+//   reset as notesReset,
+// } from '../features/notes/noteSlice'
+import { useParams, useNavigate } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+//import NoteItem from '../components/NoteItem'
+
+function Ticket() {
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.tickets
+  )
+
+  const params = useParams()
+  const dispatch = useDispatch()
+  const { ticketId } = useParams()
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    dispatch(getTicket(ticketId))
+  }, [isError, message, ticketId])
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h3>Something went wrong</h3>
+  }
+
+  return (
+    <div className='ticket-page'>
+      <header className='ticket-header'>
+        <BackButton url='tickets' />
+        <h2>
+          Ticket ID : {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}
+        </h3>
+        <hr />
+        <div className='ticket-desc'>
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Ticket
