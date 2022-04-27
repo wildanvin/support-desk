45a4f34bf047d9e81bae5e708608851c4baaef42
wildanvin <danvin.jim@gmail.com>
2022-04-26 22:04:16 -0500

    New Ticket Page in FE

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -2,15 +2,15 @@ import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
-//import { createTicket, reset } from '../features/tickets/ticketSlice'
+import { createTicket, reset } from '../features/tickets/ticketSlice'
 import Spinner from '../components/Spinner'
-// import BackButton from '../components/BackButton'
+import BackButton from '../components/BackButton'
 
 function NewTicket() {
   const { user } = useSelector((state) => state.auth)
-  //   const { isLoading, isError, isSuccess, message } = useSelector(
-  //     (state) => state.tickets
-  //   )
+  const { isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.ticket
+  )
 
   const [name] = useState(user.name)
   const [email] = useState(user.email)
@@ -20,32 +20,32 @@ function NewTicket() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  //   useEffect(() => {
-  //     if (isError) {
-  //       toast.error(message)
-  //     }
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
 
-  //     if (isSuccess) {
-  //       dispatch(reset())
-  //       navigate('/tickets')
-  //     }
+    if (isSuccess) {
+      dispatch(reset())
+      navigate('/tickets')
+    }
 
-  //     dispatch(reset())
-  //   }, [dispatch, isError, isSuccess, navigate, message])
+    dispatch(reset())
+  }, [dispatch, isError, isSuccess, navigate, message])
 
   const onSubmit = (e) => {
     e.preventDefault()
     console.log(product)
-    // dispatch(createTicket({ product, description }))
+    dispatch(createTicket({ product, description }))
   }
 
-  //   if (isLoading) {
-  //     return <Spinner />
-  //   }
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
-      {/* <BackButton url='/' /> */}
+      <BackButton url='/' />
       <section className='heading'>
         <h1>Create New Ticket</h1>
         <p>Please fill out the form below</p>
