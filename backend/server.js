df00a0c089768df81dbd0eeeee59131c045d4a05
wildanvin <danvin.jim@gmail.com>
2022-04-28 23:36:42 -0500

    make it better

--- backend/server.js
+++ backend/server.js
@@ -18,17 +18,18 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-//Serve Frontend
+// Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-  //Set build folder as static
+  // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  )
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
 } else {
   app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome my to the API my fren' })
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
   })
 }
 
